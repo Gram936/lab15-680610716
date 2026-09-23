51c7ccd0b7cbbafd7f67f024efc1f079975652f6
Gram936 <jfjik55120@gmail.com>
2026-09-23 18:29:56 +0700

    com3

--- src/components/register-dialog.tsx
+++ src/components/register-dialog.tsx
@@ -85,7 +85,7 @@ export function RegisterDialog({ availableCourses, onConfirm }: RegisterDialogPr
 
           <div className="space-y-2">
             <Label htmlFor="course">วิชา</Label>
-            <Select value={courseId} onValueChange={setCourseId}>
+            <Select value={courseId} onValueChange={(value) => setCourseId(String(value))}>
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="เลือกวิชา" />
               </SelectTrigger>
